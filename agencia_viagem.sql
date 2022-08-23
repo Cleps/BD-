1059f64cc746e17afdeeb24e308e53490c568a33
Cleps <104847424+Cleps@users.noreply.github.com>
2022-08-23 09:09:52 -0500

    atualizando atv

--- agencia_viagem.sql
+++ agencia_viagem.sql
@@ -52,7 +52,37 @@ CREATE TABLE pagamento(
     pag_tipo VARCHAR(12)
 );
 
+CREATE TABLE pag_cheque(
+    venda_code CHAR(12),
+    pag_num_parcela CHAR(2),
+    cheque_num CHAR(8) PRIMARY KEY NOT NULL,
+    cheque_banco VARCHAR(12),
+    cheque_data_deposito CHAR(8)
+);
 
 
+INSERT INTO operadora(ope_codigo, ope_nome, ope_endereco, ope_code_cidade, ope_tele, ope_contato) VALUES ('00000000', 'nome A', 'enderço A', "0000", "6800000000", "contato A");
+INSERT INTO operadora(ope_codigo, ope_nome, ope_endereco, ope_code_cidade, ope_tele, ope_contato) VALUES ('11111111', 'nome B', 'enderço B', "1111", "6811111111", "contato B");
+INSERT INTO operadora(ope_codigo, ope_nome, ope_endereco, ope_code_cidade, ope_tele, ope_contato) VALUES ('22222222', 'nome C', 'enderço C', "2222", "6822222222", "contato C");
 
+INSERT INTO cidade(cid_code, cid_nome, cid_estado) VALUES ("code-00", 'cidade A', 'estado A');
+INSERT INTO cidade(cid_code, cid_nome, cid_estado) VALUES ("code-11", 'cidade Marilia', 'estado B');
+INSERT INTO cidade(cid_code, cid_nome, cid_estado) VALUES ("code-22", 'cidade C', 'estado C');
 
+INSERT INTO cliente(cli_nome, cli_cpf, cli_end, cid_code) VALUES ('cliente A', '000000000000', 'endereço Y', 'code-00');
+INSERT INTO cliente(cli_nome, cli_cpf, cli_end, cid_code) VALUES ('cliente B', '121212121212', 'endereço Z', 'code-11');
+INSERT INTO cliente(cli_nome, cli_cpf, cli_end, cid_code) VALUES ('cliente C', '131313131313', 'endereço X', 'code-22');
+INSERT INTO cliente(cli_nome, cli_cpf, cli_end, cid_code) VALUES ('cliente D', '151515151515', 'endereço X', 'code-22');
+
+SELECT cli_nome, cli_cpf as "cpf-----", cid_nome
+FROM cliente c
+    INNER JOIN /*FUNÇÃO PARA LIGAÇÃO DE ELEMENTOS NA TABELA*/
+    cidade ci
+    ON c.cid_code = ci.cid_code;
+    
+    
+    
+    
+    
+    
+    
